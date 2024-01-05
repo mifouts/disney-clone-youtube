95cca6835ffe9311e58f673741826cb37ed26d18
mifouts <fouts.mitch15@gmail.com>
2024-01-05 14:39:14 -0500

    genre search page

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,4 +1,13 @@
-function GenrePage() {
+type Props = {
+  params: {
+    id: string;
+  };
+  searchParams: {
+    genre: string;
+  };
+};
+
+function GenrePage({ params: { id }, searchParams: { genre } }: Props) {
   return <div>GenrePage</div>;
 }
 
