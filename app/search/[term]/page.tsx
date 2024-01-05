99e9406583930e171e5f1da56baa1d91504e1071
mifouts <fouts.mitch15@gmail.com>
2024-01-05 14:32:39 -0500

    making serach page dynamic

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+
 type Props = {
   params: {
     term: string;
@@ -5,7 +7,14 @@ type Props = {
 };
 
 function SearchPage({ params: { term } }: Props) {
-  return <div>Welcome to the Search Page: {term} </div>;
+  if (!term) notFound();
+
+  const termToUse = decodeURI(term);
+
+  // API call for searched movies
+  // API call for popular movies
+
+  return <div>Welcome to the Search Page: {termToUse} </div>;
 }
 
 export default SearchPage;
