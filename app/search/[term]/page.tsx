66f3846e64c80aaee412ca1c661a15287478009a
mifouts <fouts.mitch15@gmail.com>
2024-01-05 14:30:09 -0500

    search page

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -1,5 +1,11 @@
-function SearchPage() {
-  return <div>SearchPage</div>;
+type Props = {
+  params: {
+    term: string;
+  };
+};
+
+function SearchPage({ params: { term } }: Props) {
+  return <div>Welcome to the Search Page: {term} </div>;
 }
 
 export default SearchPage;
