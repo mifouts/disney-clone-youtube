6dd79990ee2972b9d4a73b4bb5fbe17fe18b9353
mifouts <fouts.mitch15@gmail.com>
2024-01-08 12:43:39 -0500

    importing header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import Header from "@/components/ui/Header";
 
 export const metadata: Metadata = {
   title: "Disney+ Clone",
@@ -14,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
