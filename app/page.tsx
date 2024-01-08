bdec4b02f9209a6975531a3325c8d8bf206c42f8
mifouts <fouts.mitch15@gmail.com>
2024-01-08 12:40:26 -0500

    installing Shadcn ui and installing button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="text-red-500">
       <h1>lets build disney</h1>
+      <Button>Click Me</Button>
     </main>
   );
 }
