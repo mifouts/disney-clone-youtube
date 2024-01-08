86a48942934fcc3583361d2a7411d07f975878c0
mifouts <fouts.mitch15@gmail.com>
2024-01-08 12:56:35 -0500

    styling page

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-white dark:bg-[#1A1C29]">
         <Header />
         {children}
       </body>
