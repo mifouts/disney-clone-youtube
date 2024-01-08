6010050714e779ea84405435297316b075d0a67b
mifouts <fouts.mitch15@gmail.com>
2024-01-08 14:32:32 -0500

    starting movie carousels

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@/components/ui/button";
+import MoviesCarousel from "@/components/MoviesCarousel";
 
 export default function Home() {
   return (
     <main className="text-red-500">
       <h1>lets build disney</h1>
+      <div className="flex flex-col space-y-2 xl:mt-48">
+        <MoviesCarousel movies={[]} title="Upcoming" />
+      </div>
     </main>
   );
 }
