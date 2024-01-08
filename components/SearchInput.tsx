a2d009614ac9fd1f17be24f3c29cb713db92a3ed
mifouts <fouts.mitch15@gmail.com>
2024-01-08 13:31:22 -0500

    creating drop down menu

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,13 +1,48 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { FormControl, FormField } from "./ui/form";
+import { Input } from "./ui/input";
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
 });
 
 function SearchInput() {
-  return <div>SearchInput</div>;
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      input: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="input"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
 }
 
 export default SearchInput;
