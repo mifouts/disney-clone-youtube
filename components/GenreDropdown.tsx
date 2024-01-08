a2f80afa8e8ddb2c3d68837b5f9d5475cd646ea4
mifouts <fouts.mitch15@gmail.com>
2024-01-08 13:57:26 -0500

    creating api request for TMDB

---
+++ components/GenreDropdown.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function GenreDropdown() {
+  const url = "https://api.themoviedb.org/3/genre/movie/list?language=en";
+  const options: RequestInit = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+    },
+  };
+
+  return <div>GenreDropdown</div>;
+}
+
+export default GenreDropdown;
