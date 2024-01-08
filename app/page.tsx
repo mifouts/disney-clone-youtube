6566930fa135b6bf5df6efe5e92f2c09e92de873
mifouts <fouts.mitch15@gmail.com>
2024-01-08 14:42:34 -0500

    getting movie calls

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 import MoviesCarousel from "@/components/MoviesCarousel";
+import {
+  getPopularMovies,
+  getTopRatedMovies,
+  getUpcomingMovies,
+} from "@/lib/getMovies";
 
 export default async function Home() {
   const upcomingMovies = await getUpcomingMovies();
@@ -8,7 +13,9 @@ export default async function Home() {
     <main className="text-red-500">
       <h1>lets build disney</h1>
       <div className="flex flex-col space-y-2 xl:mt-48">
-        <MoviesCarousel movies={[]} title="Upcoming" />
+        <MoviesCarousel movies={upcomingMovies} title="Upcoming" />
+        <MoviesCarousel movies={topRatedMovies} title="Upcoming" />
+        <MoviesCarousel movies={popularMovies} title="Upcoming" />
       </div>
     </main>
   );
