b8d94645e2c00e4e4df1f11a944d70a0a3b7a6d6
mifouts <fouts.mitch15@gmail.com>
2024-01-16 14:49:10 -0500

    styling movie card component

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -7,6 +7,7 @@ function MovieCard({ movie }: { movie: Movie }) {
     <div>
       <p>{movie.title}</p>
       <Image
+        className="w-fit lg:min-w-[400px] h-56 object-cover object-center shadow-md shadow-gray-900 drop-shadow-xl rounded-sm"
         src={getImagePath(movie.backdrop_path || movie.poster_path)}
         alt={movie.title}
         width={1920}
