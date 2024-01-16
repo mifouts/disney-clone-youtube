e9dcfd5c8818478cf8ea20a6e193af01a47b38af
mifouts <fouts.mitch15@gmail.com>
2024-01-16 15:06:01 -0500

    trying to hide scrollbar

--- components/MoviesCarousel.tsx
+++ components/MoviesCarousel.tsx
@@ -15,7 +15,7 @@ function MoviesCarousel({ title, movies, isVertical }: Props) {
 
       <div
         className={cn(
-          "flex space-x-4 overflow-scroll scrollbar-hide px-5 lg:px-10 py-5 "
+          "flex space-x-4 overflow-scroll px-5 lg:px-10 py-5 scrollbar-hide"
         )}
       >
         {movies?.map((movie) => (
