aefff0ea281f9a0d9e2e66b0ce11ff254d4918e3
mifouts <fouts.mitch15@gmail.com>
2024-01-16 14:57:06 -0500

    creating gradient on movie card

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 function MovieCard({ movie }: { movie: Movie }) {
   return (
     <div className="relative">
+      <div className="absolute inset-0 bg-gradient-to-b from-gray-200/0 via-gray-900/10 to-gray-300 dark:to-[#1A1C29]/80 z-10" />
       <p className="absolute z-20 bottom-5 left-5">{movie.title}</p>
       <Image
         className="w-fit lg:min-w-[400px] h-56 object-cover object-center shadow-md shadow-gray-900 drop-shadow-xl rounded-sm"
