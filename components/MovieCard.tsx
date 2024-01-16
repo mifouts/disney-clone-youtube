09b17b519d56b54748b68429f55c0f67a6e11dfb
mifouts <fouts.mitch15@gmail.com>
2024-01-16 14:40:08 -0500

    rendering the movies

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,9 +1,17 @@
 import { Movie } from "@/typings";
+import Image from "next/image";
 
 function MovieCard({ movie }: { movie: Movie }) {
   return (
     <div>
       <p>{movie.title}</p>
+      <Image
+        src={getImagePath(movie.backdrop_path || movie.poster_path)}
+        alt={movie.title}
+        width={1920}
+        height={1080}
+        key={movie.id}
+      />
     </div>
   );
 }
